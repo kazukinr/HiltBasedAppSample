051996813b3b3d2e662452bc4a3d0e052fb3635c
Kazuki Nara <kazuki.nara@gmail.com>
2020-11-21 23:11:14 +0900

    Add Hilt EntryPoint.

---
+++ app/src/main/java/com/github/kazukinr/android/example/hilt/App.kt
@@ -0,0 +1,8 @@
+package com.github.kazukinr.android.example.hilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+}
--- app/src/main/java/com/github/kazukinr/android/example/hilt/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/github/kazukinr/android/example/hilt/ui/dashboard/DashboardFragment.kt
@@ -9,18 +9,20 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.github.kazukinr.android.example.hilt.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
